e433f50d4132d582c1b64529ff3aec4d243696f6
Mehul Gupta <111681702+Diablo2004@users.noreply.github.com>
2024-02-29 12:27:41 +0530

    Time: 0 ms (100.00%), Space: 13.4 MB (7.17%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
@@ -13,38 +13,37 @@ class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
         vector<vector<int>> result;
-        if(!root){
+        if (!root) {
             return result;
         }
         queue<TreeNode*> q;
-        int level=0;
+        int level = 0;
         q.push(root);
-                    while (!q.empty()) {
-                        int levelSize = q.size();
-                        deque<int> currentLevel;
-                        for (int i = 0; i < levelSize; ++i) {
-                            TreeNode* node = q.front();
-                            q.pop();
-                            if (level % 2 == 0) {
-                                currentLevel.push_back(node->val);
-                            } else {
-                                currentLevel.push_front(node->val);
-                            }
-                            if (node->left) {
-                                q.push(node->left);
-                            }
-                            if (node->right) {
-                                q.push(node->right);
-                            }
-                        }
-                        vector<int> currentLevelVec;
-                        for (auto& val : currentLevel) {
-                            currentLevelVec.push_back(val);
-                        }
-                        result.push_back(currentLevelVec);
-                        level++;
-                    }
-        
+        while (!q.empty()) {
+            int levelSize = q.size();
+            deque<int> currentLevel;
+            for (int i = 0; i < levelSize; ++i) {
+                TreeNode* node = q.front();
+                q.pop();
+                if (level % 2 == 0) {
+                    currentLevel.push_back(node->val);
+                } else {
+                    currentLevel.push_front(node->val);
+                }
+                if (node->left) {
+                    q.push(node->left);
+                }
+                if (node->right) {
+                    q.push(node->right);
+                }
+            }
+            vector<int> currentLevelVec;
+            for (auto& val : currentLevel) {
+                currentLevelVec.push_back(val);
+            }
+            result.push_back(currentLevelVec);
+            level++;
+        }
         return result;
     }
 };

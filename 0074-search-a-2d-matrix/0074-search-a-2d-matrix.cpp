e555e4303ebba19ca58a75d7dbd155240f34c8b4
Mehul Gupta <111681702+Diablo2004@users.noreply.github.com>
2024-03-03 10:53:38 +0530

    Time: 3 ms (68.98%), Space: 12 MB (7.25%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -8,34 +8,35 @@ public:
             return false;
         if (target < matrix[0][0] || target > matrix[rows - 1][columns - 1])
             return false;
-        
-        int left = 0;
-        int right = rows - 1;
+        int left=0;
+        int right=matrix.size()-1;
         int column;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (matrix[mid][0] <= target) {
+        while(left<=right){
+            int mid= left+(right-left)/2;
+            if(matrix[mid][0]<=target){
                 if (mid == rows - 1 || matrix[mid + 1][0] > target) {
                     column = mid;
                     break;
-                } else {
-                    left = mid + 1;
                 }
-            } else {
-                right = mid - 1;
+            }
+            if(matrix[mid][0]<target){
+                left=mid+1;
+            }else{
+                right=mid-1;
             }
         }
+        left=0;
         
-        left = 0;
-        right = columns - 1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (matrix[column][mid] == target) {
+        right=matrix[0].size()-1;
+        while(left<=right){
+            int mid= left+(right-left)/2;
+            if(matrix[column][mid]==target){
                 return true;
-            } else if (matrix[column][mid] < target) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
+            }
+            else if(matrix[column][mid]<target){
+                left=mid+1;
+            }else{
+                right=mid-1;
             }
         }
         return false;

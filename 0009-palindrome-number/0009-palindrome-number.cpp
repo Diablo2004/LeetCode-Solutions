42ddb1a942d19e77d08d49501f09c782259eae24
Mehul Gupta <111681702+Diablo2004@users.noreply.github.com>
2024-03-06 12:39:27 +0530

    Time: 16 ms (15.23%), Space: 8.2 MB (38.29%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if(x<0){
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
             return false;
         }
-        string str=to_string(x);
-        int left=0;
-        int right=str.length()-1;
-        while(left<right){
-            if(str[left]!=str[right]){
-                return false;
-            }
-            left++;
-            right--;
+
+        int reversed = 0;
+        while (x > reversed) {
+            reversed = reversed * 10 + x % 10;
+            x /= 10;
         }
-        return true;
+        return x == reversed || x == reversed / 10;
     }
 };

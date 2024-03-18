be0130ae5ba233fcc2bc8e914ea8544776615e11
Mehul Gupta <111681702+Diablo2004@users.noreply.github.com>
2024-03-18 15:37:57 +0530

    Time: 258 ms (55.66%), Space: 93.2 MB (65.76%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
@@ -5,20 +5,19 @@ public:
         sort(points.begin(), points.end(), [](const auto& a, const auto& b) {
             return a[1] < b[1];
         });
-        vector<vector<int>> ans;
         int i=0;
         int n=points.size();
+        int ans=1;
         while(i<n-1){
             if(points[i][1]>=points[i+1][0]){
                 points[i+1][1]=min(points[i][1],points[i+1][1]);
                 
             }else{
-                ans.push_back(points[i]);
+                ans++;
             }
             
             i++;            
         }
-        ans.push_back(points[i]);
-        return ans.size();
+        return ans;
     }
 };

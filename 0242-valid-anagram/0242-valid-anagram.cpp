3a7feb33c21d12f16b0ee0872d563b92821ec459
Mehul Gupta <111681702+Diablo2004@users.noreply.github.com>
2024-03-18 20:03:27 +0530

    Time: 12 ms (29.48%), Space: 9.7 MB (17.77%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char,int> map;
+        for(auto c:s){
+            map[c]++;
+        }
+        for(auto c:t){
+            map[c]--;
+        }
+        for(auto& pair:map){
+            if(pair.second!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+};

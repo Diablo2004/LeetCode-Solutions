87c3469a8cc2d90814f1f6e7b04db8b4ead34fa6
Mehul Gupta <111681702+Diablo2004@users.noreply.github.com>
2024-06-08 11:09:06 +0530

    Time: 56 ms (61.08%), Space: 53.3 MB (92.03%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -4,11 +4,8 @@ public:
         int n= size(nums);
        
         for(int i=0;i<n;i++){
-            int x=nums[i]; // x = current element
+            int x=nums[i]; 
             
-        // x>=1 && x<=n : to check if x is in range[1, n]
-        // x != i+1 : skip if at index i correct element is present.
-        // nums[x-1]!=x: skip if at index x-1 correct element is present
             while(x>=1 && x<=n && x!=i+1 && nums[x-1]!=x){
                 swap(nums[x-1],nums[i]);
                 x=nums[i];
@@ -18,8 +15,7 @@ public:
 
         for(int i=0;i<n;i++){
             if(nums[i] == i+1)continue;
-                return i+1;       
-            
+                return i+1;      
         }
         
         return n+1;

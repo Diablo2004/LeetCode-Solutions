f4936dcfcbbf63c92428c5175d8133bd7fe0ff01
Mehul Gupta <111681702+Diablo2004@users.noreply.github.com>
2024-06-09 11:30:58 +0530

    Time: 55 ms (18.26%), Space: 35.1 MB (63.15%) - LeetHub

--- 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.cpp
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.cpp
@@ -1,33 +1,19 @@
 class Solution {
 public:
-    int subarraysDivByK(std::vector<int>& nums, int k) {
-        // Initialize count of subarrays, prefix sum, and hash map for remainders
-        int count = 0;
-        int prefixSum = 0;
-        std::unordered_map<int, int> prefixMap;
-        prefixMap[0] = 1; // To handle subarrays that start from the beginning
-
-        for (int num : nums) {
-            // Update prefix sum
-            prefixSum += num;
-            
-            // Calculate the remainder of the prefix sum divided by k
-            int mod = prefixSum % k;
-            
-            // Adjust negative remainders to be positive
-            if (mod < 0) {
-                mod += k;
+    int subarraysDivByK(vector<int>& nums, int k) {
+        unordered_map<int,int> map;
+        map[0]++;
+        int sum=0;
+        int cnt=0;
+        for(int i=0;i<nums.size();i++){
+            sum=(sum+nums[i])%k;
+            if(sum<0) sum+=k;
+            if(map.find(sum)!=map.end()){
+                cnt+=map[sum];
             }
-            
-            // If this remainder has been seen before, it means there are subarrays ending here that are divisible by k
-            if (prefixMap.find(mod) != prefixMap.end()) {
-                count += prefixMap[mod];
-                prefixMap[mod] += 1;
-            } else {
-                prefixMap[mod] = 1;
-            }
+            map[sum]++;
         }
         
-        return count; // Total number of subarrays divisible by k
+    return cnt;
     }
 };
